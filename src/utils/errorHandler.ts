bbceeca8079f1594e29955a86d6c61eac4cbb165
Frances Reagan James <adtgwen@gmail.com>
2025-06-05 21:52:18 -0400

    added validationerror and networkerror error handlers

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -6,3 +6,22 @@ export class ApiError extends Error {
     this.name = "ApiError";
    }
 }
+
+//custom error class for validation errors//
+export class ValidationError extends Error {
+  constructor(
+    message:string,
+    public field?:string,
+  ) {
+    super(message);
+    this.name="ValidationError";
+  }
+}
+
+//custom error classfor network errors//
+export class NetworkError extends Error {
+  constructor(message:string) {
+    super(message);
+    this.name = "NetworkError";
+  }
+}
